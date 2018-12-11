21106018160356cdd87c615cbbee64dfd8f75d77
Vladislav Trofimov <stranger.65536@gmail.com>
2018-12-12 01:20:33 +0300

    run script exit status; optional pause

--- runtests.sh
+++ runtests.sh
@@ -4,7 +4,9 @@ function pause() {
     read -n1 -r -p "Press any key to continue..." key
 }
 
-java -Dfile.encoding=UTF8 -cp artifacts/*:lib/*:out info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.kuplkri.walk.Walk
+java -Dfile.encoding=UTF8 -cp artifacts/*:lib/*:out info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.kuplkri.walk.RecursiveWalk
+STATUS="${?}"
 if [ "${1}" != "" ]; then
     pause
 fi
+exit "${STATUS}"
