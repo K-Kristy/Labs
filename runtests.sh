cf256c50e1fcc9f81b114cd799d0dbd848f061b3
Vladislav Trofimov <stranger.65536@gmail.com>
2018-12-12 00:35:28 +0300

    fixed scripts

--- runtests.sh
+++ runtests.sh
@@ -5,4 +5,6 @@ function pause() {
 }
 
 java -Dfile.encoding=UTF8 -cp artifacts/*:lib/*:out info.kgeorgiy.java.advanced.walk.Tester RecursiveWalk ru.ifmo.ctddev.kuplkri.walk.Walk
-pause
+if [ "${1}" != "" ]; then
+    pause
+fi
